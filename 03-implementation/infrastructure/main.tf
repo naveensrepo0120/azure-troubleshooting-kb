1c4456400ed16471e41715fd8d9238b0eca5557c
shockwave <52635169+naveensrepo0120@users.noreply.github.com>
2026-02-10 17:01:34 +0530

    Update main.tf

--- 03-implementation/infrastructure/main.tf
+++ 03-implementation/infrastructure/main.tf
@@ -9,7 +9,7 @@ resource "azurerm_app_service_plan" "plan" {
   resource_group_name = azurerm_resource_group.rg.name
 
   sku {
-    tier = "Basic"
+    tier = "Shared"
     size = var.app_service_sku
   }
 }
