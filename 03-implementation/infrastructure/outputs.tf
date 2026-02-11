dedd963fd3c304e4ac2761527314525bfcd10a31
shockwave <52635169+naveensrepo0120@users.noreply.github.com>
2026-02-11 10:44:11 +0530

    Update outputs.tf

--- 03-implementation/infrastructure/outputs.tf
+++ 03-implementation/infrastructure/outputs.tf
@@ -3,13 +3,25 @@ output "resource_group_name" {
 }
 
 output "backend_api_url" {
-  value = azurerm_app_service.api.default_site_hostname
+  value = azurerm_linux_web_app.api.default_hostname
 }
 
 output "frontend_web_url" {
-  value = azurerm_app_service.web.default_site_hostname
+  value = azurerm_linux_web_app.web.default_hostname
 }
 
 output "backend_managed_identity_principal_id" {
-  value = azurerm_app_service.api.identity[0].principal_id
+  value = azurerm_linux_web_app.api.identity[0].principal_id
+}
+
+output "cosmos_endpoint" {
+  value = azurerm_cosmosdb_account.cosmos.endpoint
+}
+
+output "search_service_name" {
+  value = azurerm_search_service.search.name
+}
+
+output "search_endpoint" {
+  value = "https://${azurerm_search_service.search.name}.search.windows.net"
 }
