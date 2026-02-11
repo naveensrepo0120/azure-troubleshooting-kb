28b92b1165386065e2717957e6e8126050647ef9
shockwave <52635169+naveensrepo0120@users.noreply.github.com>
2026-02-11 10:44:41 +0530

    Update variables.tf

--- 03-implementation/infrastructure/variables.tf
+++ 03-implementation/infrastructure/variables.tf
@@ -1,3 +1,13 @@
+variable "location" {
+  description = "Azure region"
+  type        = string
+}
+
+variable "environment" {
+  description = "Deployment environment"
+  type        = string
+}
+
 variable "project_name" {
   description = "Short project name"
   type        = string
@@ -6,5 +16,16 @@ variable "project_name" {
 variable "app_service_sku" {
   description = "App Service Plan SKU"
   type        = string
-  default     = "D1"
+  default     = "B1"
 }
+
+variable "cosmos_account_name" {
+  description = "Cosmos DB account name"
+  type        = string
+}
+
+variable "search_service_name" {
+  description = "Azure Cognitive Search service name"
+  type        = string
+}
+
